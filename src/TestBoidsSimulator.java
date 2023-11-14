28d43da509e6004d6c5fa9f11d346ca63bf2bbdf
laulelien <aurelien.dlry@gmail.com>
2023-11-14 13:42:21 +0100

    Made wall repulsion bigger

--- src/TestBoidsSimulator.java
+++ src/TestBoidsSimulator.java
@@ -2,9 +2,9 @@ import java.awt.*;
 
 public class TestBoidsSimulator {
     public static void main(String[] args) {
-        BoidCaracteristics blueCaracteristics = new BoidCaracteristics(Color.BLUE, 1, 12, 2, 0.03, 0.1, 100, 100, 0.2, 2);
-        BoidCaracteristics redCaracteristics = new BoidCaracteristics(Color.RED, 10, 30, 1, 0.01, 0.05, 300, 50, 0.1, 4);
-        BoidCaracteristics greenCaracteristics = new BoidCaracteristics(Color.GREEN, 0.1, 3, 4, 0.05, 0.02, 10, 20, 0.3, 1);
+        BoidCaracteristics blueCaracteristics = new BoidCaracteristics(Color.BLUE, 1, 12, 2, 0.03, 0.1, 100, 30, 0.2, 2);
+        BoidCaracteristics redCaracteristics = new BoidCaracteristics(Color.RED, 10, 30, 1, 0.01, 0.05, 300, 20, 0.1, 4);
+        BoidCaracteristics greenCaracteristics = new BoidCaracteristics(Color.GREEN, 0.1, 3, 4, 0.05, 0.02, 10, 10, 0.3, 1);
         Boids boids = new Boids(500, 20);
         boids.addBoidGroup(30, blueCaracteristics);
         boids.addBoidGroup(15, redCaracteristics);
