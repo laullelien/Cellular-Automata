8af2098d5b1b3014150ecc7cbdbb5e0fc0d0e23b
chenevois <augustin.chenevois@gmail.com>
2023-11-17 15:06:10 +0100

    testGameOfLife update

--- src/TestGameOfLifeSimulator.java
+++ src/TestGameOfLifeSimulator.java
@@ -1,40 +1,41 @@
 import java.awt.Color;
-class Test1_StaticCells {
+
+class Test1_StandardCells {
     public static void main(String[] args) {
-        Color[] colors = {new Color(10, 10, 100), new Color(255, 255, 255)};
-        GameOfLifeSimulator game = new GameOfLifeSimulator(4, 2, new int[]{ /* Define numberNeighborsToLive */ }, new int[]{ /* Define numberNeighborsToBorn */ }, colors[0], colors[1]);
+        Color[] colors = {new Color(0, 128, 255), new Color(255, 255, 255)};
+        GameOfLifeSimulator game = new GameOfLifeSimulator(400, 20, new int[]{2, 3}, new int[]{3}, colors[0], colors[1]);
         // Additional code specific to Test 1 if needed
     }
 }
 
-class Test2_BlinkerCells {
+class Test2_StaticCells {
     public static void main(String[] args) {
-        Color[] colors = {new Color(10, 10, 100), new Color(255, 255, 255)};
-        GameOfLifeSimulator game = new GameOfLifeSimulator(11, 2, new int[]{ /* Define numberNeighborsToLive */ }, new int[]{ /* Define numberNeighborsToBorn */ }, colors[0], colors[1]);
+        Color[] colors = {new Color(0, 128, 255), new Color(255, 255, 255)};
+        GameOfLifeSimulator game = new GameOfLifeSimulator(600, 30, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8}, new int[]{}, colors[0], colors[1]);
         // Additional code specific to Test 2 if needed
     }
 }
 
-class Test3_GliderCells {
+class Test3_TwoStatesCells {
     public static void main(String[] args) {
-        Color[] colors = {new Color(10, 10, 100), new Color(100, 0, 200), new Color(190, 0, 200), new Color(255, 255, 255)};
-        GameOfLifeSimulator game = new GameOfLifeSimulator(11, 4, new int[]{ /* Define numberNeighborsToLive */ }, new int[]{ /* Define numberNeighborsToBorn */ }, colors[0], colors[3]);
+        Color[] colors = {new Color(0, 128, 255), new Color(255, 165, 0)};
+        GameOfLifeSimulator game = new GameOfLifeSimulator(400, 20, new int[]{}, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8}, colors[0], colors[1]);
         // Additional code specific to Test 3 if needed
     }
 }
-
-class Test4_OscillatingCells {
+class Test4_CellsCondemnedToConverge {
     public static void main(String[] args) {
-        Color[] colors = {new Color(10, 10, 100), new Color(100, 0, 200), new Color(255, 255, 255)};
-        GameOfLifeSimulator game = new GameOfLifeSimulator(50, 3, new int[]{ /* Define numberNeighborsToLive */ }, new int[]{ /* Define numberNeighborsToBorn */ }, colors[0], colors[2]);
+        Color[] colors = {new Color(0, 128, 255), new Color(128, 0, 200)};
+        GameOfLifeSimulator game = new GameOfLifeSimulator(800, 40, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8}, new int[]{0, 3, 4, 8}, colors[0], colors[1]);
         // Additional code specific to Test 4 if needed
     }
 }
 
-class Test5_ExpandingGrid {
+
+class Test5_CellsCondemnedToDie {
     public static void main(String[] args) {
-        Color[] colors = {new Color(10, 10, 100), new Color(100, 0, 200), new Color(255, 255, 255)};
-        GameOfLifeSimulator game = new GameOfLifeSimulator(10, 3, new int[]{ /* Define numberNeighborsToLive */ }, new int[]{ /* Define numberNeighborsToBorn */ }, colors[0], colors[2]);
+        Color[] colors = {new Color(0, 255, 0), new Color(255, 69, 0)};
+        GameOfLifeSimulator game = new GameOfLifeSimulator(400, 20, new int[]{}, new int[]{}, colors[0], colors[1]);
         // Additional code specific to Test 5 if needed
     }
 }
